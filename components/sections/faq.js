6f4468c622a8ff9d27aed37e6aad43f36623c788
Camden Phalen <camdenphalen@gmail.com>
2019-06-17 00:48:47 -0400

    faq section

--- components/sections/faq.js
+++ components/sections/faq.js
@@ -1,7 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-import { Section } from "../../style";
+import { Section, Container, H2, H6, P, Colors } from "../../style";
 
-const FAQ = ({ data }) => <Section data={data} />;
+const Questions = styled.div`
+  padding-top: 40px;
+  h6,
+  p {
+    color: ${Colors.BLACK};
+  }
+`;
+
+const Question = styled.div`
+  margin-bottom: 40px;
+`;
+
+const FAQ = ({ data }) => (
+  <Section data={data}>
+    <Container>
+      <H2>{data.title}</H2>
+      <Questions>
+        {data.questions.map((question, index) => (
+          <Question>
+            <H6>{question.question}</H6>
+            <P>{question.answer}</P>
+          </Question>
+        ))}
+      </Questions>
+    </Container>
+  </Section>
+);
 
 export default FAQ;
